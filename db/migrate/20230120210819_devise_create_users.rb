c87796fb28e816fb1246f70dc654b187ff35eeb0
doncan.orozco <doncan.orozco@gmail.com>
2023-03-23 21:21:07 -0600

    Add username and full_name attributes in user

--- db/migrate/20230120210819_devise_create_users.rb
+++ db/migrate/20230120210819_devise_create_users.rb
@@ -6,6 +6,8 @@ class DeviseCreateUsers < ActiveRecord::Migration[7.0]
       ## Database authenticatable
       t.string :email, null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
+      t.string :username, null: false, default: ""
+      t.string :full_name, null: false, default: ""
 
       ## Recoverable
       t.string :reset_password_token
@@ -37,6 +39,7 @@ class DeviseCreateUsers < ActiveRecord::Migration[7.0]
 
     add_index :users, :email, unique: true
     add_index :users, :reset_password_token, unique: true
+    add_index :users, :username, unique: true
     # add_index :users, :confirmation_token,   unique: true
     # add_index :users, :unlock_token,         unique: true
   end
--- db/schema.rb
+++ db/schema.rb
@@ -55,8 +55,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_23_225134) do
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
-    t.string "full_name", default: "", null: false
     t.string "username", default: "", null: false
+    t.string "full_name", default: "", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"

01fbe5cce50dd962da2a87ab6ab94523e51428f9
doncan.orozco <doncan.orozco@gmail.com>
2023-08-15 06:12:44 -0600

    add some actionView helpers to ApplicationComponent

--- app/views/components/application_component.rb
+++ app/views/components/application_component.rb
@@ -2,4 +2,8 @@
 
 class ApplicationComponent < Phlex::HTML
   include Phlex::Rails::Helpers::Routes
+  include Phlex::Rails::Helpers::T
+  include Phlex::Rails::Helpers::LinkTo
+  include Phlex::Rails::Helpers::DOMID
+  include Phlex::Rails::Helpers::ButtonTo
 end
